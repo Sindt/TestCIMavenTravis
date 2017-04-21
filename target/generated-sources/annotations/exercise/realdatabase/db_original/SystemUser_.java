31ea6bd50720769d2645ddb94ce466b68bb08d57
Sindt <Sindt>
2017-04-21 13:30:59 +0200

    travis

--- target/generated-sources/annotations/exercise/realdatabase/db_original/SystemUser_.java
+++ target/generated-sources/annotations/exercise/realdatabase/db_original/SystemUser_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T12:52:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T13:30:26")
 @StaticMetamodel(SystemUser.class)
 public class SystemUser_ { 
 
